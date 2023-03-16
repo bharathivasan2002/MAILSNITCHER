95635f30c8f78024feebadaa60d41ac3343783fa
bharathivasan2002 <20ca019@sankaracollege.edu.in>
2023-03-16 17:57:19 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+function google(){
+    window.open("https://www.google.com/")
+}
+
+function microsoft(){
+    window.open("https://www.microsoft.com/en-in")
+}
+
+function yahoo(){
+    window.open("https://in.yahoo.com/")
+}
